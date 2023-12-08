fba571bc07937013142da1611839c45b9bab06cb
Greg Plumbly <greg.plumbly@gmail.com>
2023-12-08 23:00:55 +0000

    set up db and prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link'
+import { auth } from '@clerk/nextjs'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth()
+  let href = userId ? '/journal' : '/new-user'
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
@@ -8,8 +11,8 @@ export default function Home() {
         <p className="text-2xl text-white/60 mb-4">
           This is the best app for tracking your mood
         </p>
-        <Link href="/journal">
-          <button className="bg-blue-600 px-4 py-2 rounded-lg">
+        <Link href={href}>
+          <button className="bg-blue-600 px-4 py-2 rounded-lg tet-xl">
             get started
           </button>
         </Link>
